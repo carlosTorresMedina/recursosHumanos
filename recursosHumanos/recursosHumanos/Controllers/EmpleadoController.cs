268b6c4343071c22ef5a2d10cf3d6f86bda0c8ad
= <=>
2017-04-19 04:01:46 -0500

    ajuste consulta empleados

--- recursosHumanos/recursosHumanos/Controllers/EmpleadoController.cs
+++ recursosHumanos/recursosHumanos/Controllers/EmpleadoController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using recursosHumanos.Models;
 
 namespace recursosHumanos.Controllers
 {
     public class empleadoController : Controller
     {
+
+        private recursosHumanosEntities entidad = new recursosHumanosEntities();
+
         // GET: Empleado
-        public ActionResult Index()
+        public ActionResult Consultar()
         {
-            return View();
+
+            var empleados = entidad.empleado;
+
+            return View(empleados.ToList());
         }
+
+
+
     }
 }
